07a5962e1cb257826c350b8aba8f9837893b0eb2
pvashish <v7pranavi@gmail.com>
2023-07-05 14:49:30 -0400

    utils added

---
+++ app.py
@@ -0,0 +1,13 @@
+from cellSegmentation.logger import logging
+from cellSegmentation.exception import AppException
+import sys
+
+
+
+logging.info("Welcome to the custom log") 
+
+try:
+    a = 4/"6"
+
+except Exception as e:
+    raise AppException(e, sys)
